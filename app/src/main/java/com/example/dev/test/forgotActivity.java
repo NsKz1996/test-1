fcd257ed74a01346c957f467d2fb9a9290f4fbfa
Arianazaralizadeh <aria.nazaralizadeh@yahoo.com>
2018-04-19 19:49:18 +0430

    aria
    takmil forgot

--- app/src/main/java/com/example/dev/test/forgotActivity.java
+++ app/src/main/java/com/example/dev/test/forgotActivity.java
@@ -1,13 +1,37 @@
 package com.example.dev.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class forgotActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.activityfofgot_button_ersal)
+    Button ersal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot);
+
+        ButterKnife.bind(this);
+
+        ersal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(forgotActivity.this, Login.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
+
+
     }
 }

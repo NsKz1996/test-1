ab8122c7e058a8ec70019f9d3ad8a3b643e28d87
Arianazaralizadeh <aria.nazaralizadeh@yahoo.com>
2018-04-19 16:52:59 +0430

    aria
    add fefagment

--- app/src/main/java/com/example/dev/test/MainActivity.java
+++ app/src/main/java/com/example/dev/test/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.dev.test;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.framelayout,new Fragment_main_home()).commit();
+
+
+
+
+
     }
+
+
+    public void onclick(View view){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        switch (view.getId()) {
+            case R.id.btn_home:
+                transaction.replace(R.id.framelayout,new Fragment_main_home()).commit();
+                break;
+            case R.id.btn_add:
+                transaction.replace(R.id.framelayout,new Fragment_main_addpost()).commit();
+                break;
+            case R.id.btn_nofification:
+                transaction.replace(R.id.framelayout,new Fragment_main_notificatiin()).commit();
+                break;
+            case R.id.btn_profile:
+                transaction.replace(R.id.framelayout,new Fragment_main_profile()).commit();
+                break;
+
+
+
+        }}
+
+
+
+
+
+
 }

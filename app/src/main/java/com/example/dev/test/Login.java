487b27cd2dbf3a0b45b32fb28db4487ff0f32690
Amir Hossein <jobeiri_a.h@yahoo.com>
2018-04-19 15:37:37 +0430

    AH

--- app/src/main/java/com/example/dev/test/Login.java
+++ app/src/main/java/com/example/dev/test/Login.java
@@ -1,15 +1,42 @@
 package com.example.dev.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Login extends AppCompatActivity {
 
-
+    @BindView(R.id.Login_EditText_UserName)
+    EditText userName;
+    @BindView(R.id.Login_EditText_Password)
+    EditText password;
+    @BindView(R.id.Login_Button_SignIn)
+    EditText signIn;
+    @BindView(R.id.Login_TextView_SignUp)
+    EditText signUp;
+    @BindView(R.id.Login_TextView_Forget)
+    EditText forgetPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/dev/test/Register.java
+++ app/src/main/java/com/example/dev/test/Register.java
@@ -2,6 +2,7 @@ package com.example.dev.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import butterknife.BindView;
@@ -9,24 +10,14 @@ import butterknife.ButterKnife;
 
 public class Register extends AppCompatActivity {
 
-    @BindView(R.id.Login_EditText_UserName)
-    EditText userName;
-    @BindView(R.id.Login_EditText_Password)
-    EditText password;
-    @BindView(R.id.Login_Button_SignIn)
-    EditText signIn;
-    @BindView(R.id.Login_TextView_SignUp)
-    EditText signUp;
-    @BindView(R.id.Login_TextView_Forget)
-    EditText forgetPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        ButterKnife.bind(this);
 
-        
+
     }
 }

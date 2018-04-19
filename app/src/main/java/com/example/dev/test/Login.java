e333bc74774257b16a12412b55d5b0a6cd53f962
Arianazaralizadeh <aria.nazaralizadeh@yahoo.com>
2018-04-19 17:03:57 +0430

    aria
    takmil login

--- app/src/main/java/com/example/dev/test/Login.java
+++ app/src/main/java/com/example/dev/test/Login.java
@@ -41,5 +41,18 @@ public class Login extends AppCompatActivity {
 
             }
         });
+
+
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
 }

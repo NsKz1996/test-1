70acc17228ddcf9a794e73e8f9af04f9c02bb442
Hessam-vesal <ohessamo@gmail.com>
2018-04-19 15:43:15 +0430

    H

--- app/src/main/java/com/example/dev/test/Register.java
+++ app/src/main/java/com/example/dev/test/Register.java
@@ -2,11 +2,7 @@ package com.example.dev.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class Register extends AppCompatActivity {
 

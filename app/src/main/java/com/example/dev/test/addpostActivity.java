bf02c642cb8a41332bece947bedcdad2026f5467
Hessam-vesal <ohessamo@gmail.com>
2018-04-20 23:50:39 +0430

    Fix Project
    Hessam

--- app/src/main/java/com/example/dev/test/addpostActivity.java
+++ app/src/main/java/com/example/dev/test/addpostActivity.java
@@ -1,43 +1,25 @@
 package com.example.dev.test;
 
-<<<<<<< HEAD
+
 import android.content.Intent;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class addpostActivity extends AppCompatActivity {
-
-
-    @BindView(R.id.Activityaddpost_button_virayesh)
-    Button virayrsh;
-=======
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.media.Image;
-import android.net.Uri;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
-
-import de.hdodenhof.circleimageview.CircleImageView;
 
 public class addpostActivity extends AppCompatActivity {
 
     ImageView imageView;
     Uri uri;
->>>>>>> 1a4632300708c69ceaadc2b2fd33814f78987bb8
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,74 +27,15 @@ public class addpostActivity extends AppCompatActivity {
         setContentView(R.layout.activity_addpost);
         ButterKnife.bind(this);
 
-        virayrsh.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(addpostActivity.this,video_editor.class);
-                startActivity(intent);
-
-            }
-        });
-
-        imageView = (ImageView) findViewById(R.id.fragmentaddpost_Imageview_addpost);
-
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    Intent intent = new Intent(Intent.ACTION_PICK,
-                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    // intent.setType("image/*");
-                    startActivityForResult(intent, 2);
-                } catch (Exception x) {
-                    Toast.makeText(getApplicationContext(), "Error Choose Image", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
+//        virayrsh.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                Intent intent = new Intent(addpostActivity.this,video_editor.class);
+//                startActivity(intent);
+//
+//            }
+//        });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == 2 && resultCode == RESULT_OK && null != data) {
-
-            uri = data.getData();
-            CropImage();
-
-        } else if (requestCode == 1) {
-            if (data != null) {
-                Bundle bundle = data.getExtras();
-                Bitmap bitmap = bundle.getParcelable("data");
-                imageView.setImageBitmap(bitmap);
-
-
-            }
-        }
-    }
-
-    private void CropImage() {
-
-        try {
-            Intent CropIntent = new Intent("com.android.camera.action.CROP");
-            CropIntent.setDataAndType(uri, "image/*");
-
-            CropIntent.putExtra("crop", "true");
-            CropIntent.putExtra("outputX", 500);
-            CropIntent.putExtra("outputY", 500);
-            CropIntent.putExtra("aspectX", 1);
-            CropIntent.putExtra("aspectY", 1);
-            CropIntent.putExtra("scaleUpIfNeeded", true);
-            CropIntent.putExtra("return-data", true);
-
-            Log.i("Crop", uri.toString());
-
-            startActivityForResult(CropIntent, 1);
-        } catch (ActivityNotFoundException ex) {
-
-        }
-
-    }
 }

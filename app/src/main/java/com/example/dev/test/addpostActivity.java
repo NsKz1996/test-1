1a4632300708c69ceaadc2b2fd33814f78987bb8
Amir Hossein <jobeiri_a.h@yahoo.com>
2018-04-20 16:49:55 +0430

    Amir Hossein
    photo of add photo

--- app/src/main/java/com/example/dev/test/addpostActivity.java
+++ app/src/main/java/com/example/dev/test/addpostActivity.java
@@ -1,15 +1,88 @@
 package com.example.dev.test;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class addpostActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addpost);
 
+        imageView = (ImageView) findViewById(R.id.fragmentaddpost_Imageview_addpost);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Intent.ACTION_PICK,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    // intent.setType("image/*");
+                    startActivityForResult(intent, 2);
+                } catch (Exception x) {
+                    Toast.makeText(getApplicationContext(), "Error Choose Image", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 2 && resultCode == RESULT_OK && null != data) {
+
+            uri = data.getData();
+            CropImage();
+
+        } else if (requestCode == 1) {
+            if (data != null) {
+                Bundle bundle = data.getExtras();
+                Bitmap bitmap = bundle.getParcelable("data");
+                imageView.setImageBitmap(bitmap);
+
+
+            }
+        }
+    }
+
+    private void CropImage() {
+
+        try {
+            Intent CropIntent = new Intent("com.android.camera.action.CROP");
+            CropIntent.setDataAndType(uri, "image/*");
+
+            CropIntent.putExtra("crop", "true");
+            CropIntent.putExtra("outputX", 500);
+            CropIntent.putExtra("outputY", 500);
+            CropIntent.putExtra("aspectX", 1);
+            CropIntent.putExtra("aspectY", 1);
+            CropIntent.putExtra("scaleUpIfNeeded", true);
+            CropIntent.putExtra("return-data", true);
+
+            Log.i("Crop", uri.toString());
+
+            startActivityForResult(CropIntent, 1);
+        } catch (ActivityNotFoundException ex) {
+
+        }
 
     }
 }

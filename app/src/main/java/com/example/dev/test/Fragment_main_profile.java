7f90919336e049b63641937fd57d804f855c8ea5
Hessam-vesal <ohessamo@gmail.com>
2018-04-20 15:08:53 +0430

    Hessam

--- app/src/main/java/com/example/dev/test/Fragment_main_profile.java
+++ app/src/main/java/com/example/dev/test/Fragment_main_profile.java
@@ -8,9 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class Fragment_main_profile extends Fragment {
 
     public static Fragment_main_profile newInstance() {
@@ -21,7 +19,8 @@ public class Fragment_main_profile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        
+
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 

10400c6b682c8a0aad6597c62720aa2f09753bf7
Amir Hossein <jobeiri_a.h@yahoo.com>
2018-04-20 11:12:01 +0430

    Amir Hossein
    add recycler view to fragment_home

--- app/src/main/java/com/example/dev/test/Fragment_main_home.java
+++ app/src/main/java/com/example/dev/test/Fragment_main_home.java
@@ -3,9 +3,16 @@ package com.example.dev.test;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerViewAccessibilityDelegate;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -14,13 +21,44 @@ import android.view.ViewGroup;
 public class Fragment_main_home extends Fragment {
 
 
-
+    private List<Model_Post_Home> model_post_homes = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private RecyclerViewHomeAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.Fragment_Main_Home_RecyclerView);
+
+        mAdapter = new RecyclerViewHomeAdapter(model_post_homes);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        prepareMovieData();
+
+        return view;
+    }
+
+    private void prepareMovieData() {
+
+        Model_Post_Home model_post_home = new Model_Post_Home(R.drawable.ghoozhism, "Ghoozh", "12:14", "Khoob", "52");
+        model_post_homes.add(model_post_home);
+        model_post_homes.add(new Model_Post_Home(R.drawable.ghoozhism, "Ghoozh", "12:14", "10", "52"));
+        model_post_homes.add(new Model_Post_Home(R.drawable.ghoozhism, "Ghoozh", "12:14", "10", "52"));
+        model_post_homes.add(new Model_Post_Home(R.drawable.ghoozhism, "Ghoozh", "12:14", "10", "52"));
+        model_post_homes.add(new Model_Post_Home(R.drawable.ghoozhism, "Ghoozh", "12:14", "10", "52"));
+        model_post_homes.add(new Model_Post_Home(R.drawable.ghoozhism, "Ghoozh", "12:14", "10", "52"));
+        model_post_homes.add(new Model_Post_Home(R.drawable.ghoozhism, "Ghoozh", "12:14", "10", "52"));
+        model_post_homes.add(new Model_Post_Home(R.drawable.ghoozhism, "Ghoozh", "12:14", "10", "52"));
+        model_post_homes.add(new Model_Post_Home(R.drawable.ghoozhism, "Ghoozh", "12:14", "10", "52"));
+        model_post_homes.add(new Model_Post_Home(R.drawable.ghoozhism, "Ghoozh", "12:14", "10", "52"));
+        model_post_homes.add(new Model_Post_Home(R.drawable.ghoozhism, "Ghoozh", "12:14", "10", "52"));
+
     }
 
 }
